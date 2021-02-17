e0a8859aa9a97ec1174dfdf03cbdeb2dfc42225e
Rene Pereira <rpereira@CLOSEUPBRZ.COM.BR>
2021-02-17 09:32:17 -0300

    finishing graphql for backend

--- backend/resolver/resolver.js
+++ backend/resolver/resolver.js
@@ -1,26 +1,22 @@
 const Movie =require('../model/model')
 
-const movies = [
-    { id:1, name: "John Wick Parabellum", genre:"Action", year:2019 },
-    { id:2, name: "John Wick Parabellum 2", genre:"Action", year:2019 },
-    { id:3, name: "John Wick Parabellum 3", genre:"Action", year:2019 },
-    { id:4, name: "John Wick Parabellum 4", genre:"Action", year:2019 },
-    { id:5, name: "John Wick Parabellum 5", genre:"Action", year:2019 },
-    { id:6, name: "John Wick Parabellum 6", genre:"Action", year:2019 },
-    { id:7, name: "John Wick Parabellum 7", genre:"Action", year:2019 },
-    { id:8, name: "John Wick Parabellum 8", genre:"Action", year:2019 },
-    { id:9, name: "John Wick Parabellum 9", genre:"Action", year:2019 },
-    { id:10, name: "John Wick Parabellum 10", genre:"Action", year:2019 },
-]
-
 const resolvers = {
     movies: ()=>{
-        return movies
+        return Movie.find({})
     },
-    addMovie: (args)=>{
-        return {
-            name: args.name
-        }
+
+    movieByName: ({name})=>{
+        return Movie.findOne({name:name})
+    },
+
+    addMovie: ({name, genre, year})=>{
+        const movie = new Movie({
+            name,
+            genre,
+            year
+        })
+        movie.save()
+        return movie
     }
 }
 
